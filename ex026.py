1df9d2bc684b4c1c0ec1f8c1546f681a2d2b3707
kroos <54759116+KroosBR@users.noreply.github.com>
2020-03-23 01:05:33 -0300

    Delete ex026.py

--- ex026.py
+++
@@ -1,4 +0,0 @@
-frase = input("Digite uma frase: ").strip().upper()
-print("Na frase tem {} A".format(frase.count("A")))
-print("Em sua primeira vez a palavra A aparece na posição[{}]".format(frase.find("A")+1))
-print("A ultima vez a palavra A aparece na posição[{}]".format(frase.rfind("A")+1))   
